52f3fd82e2ebc72cfe529d6f29e3a0498d8a7c22
Matt Burns <burns963@yahoo.com>
2018-10-19 23:17:22 -0500

    Added Pythoin Editor to 3.6

--- Burns_MSDS510/Baby Step.py
+++ Burns_MSDS510/Baby Step.py
@@ -1,3 +1,6 @@
 colors = "blue","red","yellow","purple"
+counter = 1
 for color in colors:
-    print ("I like",color)
+    print (counter,") I like",color)
+    counter = counter + 1
+print (counter)
