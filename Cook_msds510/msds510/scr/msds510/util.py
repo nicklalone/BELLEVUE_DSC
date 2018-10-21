046e635420ae9db55e4101d2c9c46990f043c0ce
thoehn31415 <43759380+thoehn31415@users.noreply.github.com>
2018-10-20 22:46:23 -0500

    Update Cook_msds510/msds510/scr/msds510/util.py

--- Cook_msds510/msds510/scr/msds510/util.py
+++ Cook_msds510/msds510/scr/msds510/util.py
@@ -1,7 +1,18 @@
 import datetime
-
+'''
+This is a utility module that holds some functions that are used
+to find the date the character joined as well as calculate the 
+number of days elapsed since the character joined.
+'''
 
 def get_month(intro):
+'''
+This section of code splits the intro field into two parts.
+The dictionary is used to assign months to numbers.
+Each part is then checked against the dictionary
+to determine the month value.
+'''
+
     values = intro.split('-')
     months = dict(
         Jan=1, Feb=2, Mar=3, Apr=4, May=5, Jun=6,
@@ -15,6 +26,13 @@ def get_month(intro):
 
 
 def get_date_joined(year_str, intro):
+    '''
+    This section relies on the get_month function above.
+    It takes a year and an intro argument and returns
+    a datetime object of when the character joined.
+    Since some start dates are only months, all start
+    dates are set to the first of the month.
+    '''
     month = get_month(intro)
     year = int(year_str)
 
@@ -22,6 +40,11 @@ def get_date_joined(year_str, intro):
 
 
 def days_since_joined(year, intro):
+    '''
+    This function takes the year and intro fields
+    and returns the number of days from today's
+    date that have elapsed.
+    '''
     today = datetime.date.today()
     date_joined = get_date_joined(year, intro)
     return (today-date_joined).days
