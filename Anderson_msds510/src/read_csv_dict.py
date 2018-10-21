0493667bdfe0a131e032d76edfbd0445bf63f9cb
ganga <gdhul001@fiu.edu>
2018-10-21 14:42:25 -0400

    Added required files and comments , rearranged code

--- Anderson_msds510/src/read_csv_dict.py
+++ Anderson_msds510/src/read_csv_dict.py
@@ -1,25 +1,30 @@
 import sys
 import csv
+from pprint import pprint
 
-def argumentExists(index):
-    try:
-        sys.argv[index]
-    except IndexError:
-        return ''
+
+# The following code takes the path for the input file
+# csv_DictReader gives a csv_dictreader object
+# The dictionary reader iterated to print the rows
+# Dictreader knows that the first row corresponds to filed names
+
+def csvdict_read(inputfilepath):
+    with open(inputfilepath) as f:
+        csv_dictreader = csv.DictReader(f)
+        fieldnames = csv_dictreader.fieldnames
+        rows = [row for row in csv_dictreader]
+        pprint(rows[161])
+
+# The code below is to check if there is atleast 2 arguements entered in the command line interface
+
+def main():
+    args = sys.argv
+
+    if len(args) < 1:
+        print("Please enter the path name for the two files")
     else:
-        return sys.argv[index]
+        csvdict_read(args[1])
 
-
-def readRows(inputCSV):
-    with open (inputCSV,'r') as read:
-        listOfOrderedDics = []
-        readCSV = csv.fieldnames
-        for row in readCSV:
-            listOfOrderedDics.append(row)
-        for key in keys:
-            print(key, listOfOrderedDics[160][key], end=" ")
 
 if __name__ == "__main__":
-    csvToRead = argumentExists(1)
-    readRows(csvToRead)
-# having a little trouble with this one
+      main()
