ccf4f651c9661c186c758672dde5d8a9fdd90fbc
ganga <gdhul001@fiu.edu>
2018-10-21 12:37:47 -0400

    Added Comments-gitignore and init files

--- Campbell-msds510/src/write_csv.py
+++ Campbell-msds510/src/write_csv.py
@@ -12,8 +12,17 @@ def main(input_csv, output_csv):
         records = [record for record in csv_reader]
         fieldnames = csv_reader.fieldnames
 
+    # The following line of code is a list comprehension. It creates a list of python_friendly_names
+
     python_friendly_names = [python_friendly_name(name) for name in fieldnames]
+
+    """The following line is a combination of a list and a dictionary comprehension. 
+    It takes the form of (k,v for (k,v) in collection.items)"""
+
     new_records = [{python_friendly_name(name): value for name, value in record.items()} for record in records]
+
+    """ Dict Reader Requires us to specify the field names. Especially the writeheader 
+        method is what specifies the header values"""
 
     with open(output_csv, 'w') as f:
         csv_writer = csv.DictWriter(f, fieldnames=python_friendly_names)
@@ -27,3 +36,5 @@ if __name__ == '__main__':
         print('usage: write_csv <input_csv> <output_csv>')
     else:
         main(args[1], args[2])
+
+
