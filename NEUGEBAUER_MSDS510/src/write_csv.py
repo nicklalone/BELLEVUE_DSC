daf77f94c6bd6c9ac6f02c1728af4ff0859b6f58
thoehn31415 <43759380+thoehn31415@users.noreply.github.com>
2018-10-22 12:49:50 -0500

    Updated write_csv

--- NEUGEBAUER_MSDS510/src/write_csv.py
+++ NEUGEBAUER_MSDS510/src/write_csv.py
@@ -1,2 +1,37 @@
+import sys
+import csv
 
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+#I saw that there was no code for writing the csv, so I added
+#what I came up with.  Feel free to read through and make any changes
+def make_nice_name(name):
+    new_name = []
+    for x in name:
+        temp_name = x.lower().replace(' ', '_').replace('/', '_').strip('?').strip('\n')
+        new_name.append(temp_name)
+    return new_name
+
+
+def reading(input):
+    with open(input, 'r') as f:
+        temp_file = csv.DictReader(f)
+        temp_file.fieldnames = make_nice_name(temp_file.fieldnames)
+        output_file = []
+        for row in temp_file:
+            output_file.append(row)
+        return temp_file.fieldnames, output_file
+
+
+def writer(input, output, header):
+    with open(output, 'w', newline='') as o:
+        output_writer = csv.DictWriter(o, fieldnames=header)
+        output_header = {}
+        # Want a row of the header;
+        for x in header:
+            output_header[x] = x
+        output_writer.writerow(output_header)
+        for row in input:
+            output_writer.writerow(row)
 
