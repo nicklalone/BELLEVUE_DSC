3b054a5c8c77b2b1b15e8bee7d080a0c0d786532
Matt Burns <burns963@yahoo.com>
2018-10-22 20:30:00 -0500

    Added comments to Gardner read.csv

--- Gardner_msds510/src/read_csv.py
+++ Gardner_msds510/src/read_csv.py
@@ -1,7 +1,13 @@
+## I do not comment on my code at work, because it is so simple.
+## Now I can see why it is important to provide context.
+## Would the following comments work?
+
+
+## Bring CSV in PyCharm
 import sys
 import csv
 
-
+## Determine if there is a line that need to be imported
 def argumentExists(index):
     try:
         sys.argv[index]
@@ -10,7 +16,7 @@ def argumentExists(index):
     else:
         return sys.argv[index]
 
-
+## Create and fill "rows" table
 rows = []
 def readRows(inputCSV):
     with open(inputCSV, 'r') as read:
--- Hoehn_msds510/src/msds510/util.py
+++ Hoehn_msds510/src/msds510/util.py
@@ -3,7 +3,6 @@ from datetime import datetime
 def get_month(year, intro):
     month = get_date_joined(year, intro).month
     return month
-
 def get_date_joined(year, intro):
     if intro[0] in ('1','2','3','4','5','6','7','8','9'):
         temp_date = '1 ' + intro[-3:] + ' ' + year[2:]
@@ -16,4 +15,3 @@ def days_since_joined(year,intro):
     date_joined = get_date_joined(year,intro)
     elapsed_days = datetime.today().date() - date_joined
     return elapsed_days.days
-
