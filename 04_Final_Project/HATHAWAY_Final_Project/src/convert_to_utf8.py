9715970dec6e6a797453855e924e9480675be759
chhathaway71 <43800171+chhathaway71@users.noreply.github.com>
2018-11-03 12:52:12 -0500

    conversion python file

---
+++ 04_Final_Project/HATHAWAY_Final_Project/src/convert_to_utf8.py
@@ -0,0 +1,17 @@
+import sys
+
+#create function to convert Avengers file to UTF8
+def main(input_file_path, output_file_path):
+    with open(input_file_path, encoding='ISO-8859-1 ') as f:
+        text = f.read()
+
+    with open(output_file_path, 'w', encoding='UTF8') as f:
+        f.write(text)
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) < 3:
+        print('usage: convert_to_utf8 <input_file> <output_file>')
+    else:
+        main(str(args[1]), str(args[2]))
