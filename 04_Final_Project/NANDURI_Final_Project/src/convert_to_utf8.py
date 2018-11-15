a0ce81355e3d6605d776ec62c44fa605b8de4cf9
iamnrr <n.raghuraman@gmail.com>
2018-11-14 23:41:12 -0600

    Raghu Raman Nanduri - final project

--- 04_Final_Project/NANDURI_Final_Project/src/convert_to_utf8.py
+++ 04_Final_Project/NANDURI_Final_Project/src/convert_to_utf8.py
@@ -1,14 +1,18 @@
 import sys
+from msds510 import util
+
+"""
+Execution:  python convert_to_utf8.py ../data/raw/marvel-wikia-data.csv ../data/interim/marvel-wikia-data_utf8.csv
+"""
 
 
 def  convert(inputfile, outputfile):
 
-    '''
+    """
     Function to convert data in csv file encoded in 'ISO-8859-1' to 'utf-8' and save in another csv
     Arguments: 3 -  python code file, source file with path and  target file with path
     return: none
-    Execution:  python convert_to_utf8.py ../data/raw/marvel-wikia-data.csv ../data/interim/marvel-wikia-data_utf8.csv
-    '''
+    """
 
     in_avengers = open(inputfile, encoding='iso-8859-1')
     in_data = in_avengers.read()
@@ -20,15 +24,14 @@ def  convert(inputfile, outputfile):
     in_avengers.close()
     out_avengers.close()
 
-    print('File converted with utf-8 encoding and saved in interim directory')
+    print('File converted with utf-8 encoding and saved the file %s in the %s directory' % (util.getfilename(outputfile), util.getfilepath(outputfile)))
 
 
 def main():
-
-    '''
+    """
     main function to call the convert function
     :return: none
-    '''
+    """
 
     if (len(sys.argv)) < 3:
         print('Expecting 3 arguments - python filename, source file with path, target file with path')
@@ -36,6 +39,5 @@ def main():
         convert(sys.argv[1], sys.argv[2])
 
 
-if __name__  == "__main__":
-
+if __name__ == "__main__":
     main()
