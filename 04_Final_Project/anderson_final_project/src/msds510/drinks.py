80f5489a6b4fb7ac454c19fab0fbe895bac4b3c5
Sonny <40152258+SonnyEclipse@users.noreply.github.com>
2018-11-17 17:26:31 -0600

    Comment Suggestions

--- 04_Final_Project/anderson_final_project/src/msds510/drinks.py
+++ 04_Final_Project/anderson_final_project/src/msds510/drinks.py
@@ -42,7 +42,7 @@ class Avenger:
         return self.total_litres_of_pure_alcohol_value
 
 
-
+    #Joe F.: May have to remove the '()' after country
     def __str__(self):
         # Reads the object as a string
         return self.country()
@@ -53,9 +53,12 @@ class Avenger:
         Returns:
             str: String representation of object.  Useful for debugging.
         """
-
+        
         return "Avenger(" + ",".join(key + "=" + val
                                      for key, val in self.record.items()
+                                     # Joe F.: You'll probably want to replace this with the key value for
+                                     # whatever you're wanting to title your report; 'name_alias' 
+                                     # comes from the original data set for Avengers
                                      if key == 'name_alias'
                                      or key == 'url') + ")"
 
@@ -67,8 +70,14 @@ class Avenger:
             ofile.write('# List of the 10 courtries with the highest amount of liters of pure alcohol consumed.\n\n')
             for idx, rc in enumerate(records_list, 1):
                 avenger = Avenger(rc)
+                
+                #Joe F.: I had issues (see Need Help or Slack) with having the '()' 
+                # after my dc_characters.something here. May need to remove your 
+                # parentheses if you're getting an error about str type not callable, etc.
                 ofile.write('## ' + str(idx) + ': ' + avenger.country() + '\n\n')
                 ofile.write('* Liters of Beer: ' + avenger.beer_served() + '\n')
                 ofile.write('* Liters of Spirit: ' + avenger.spirit_served() + '\n')
                 ofile.write('* Liters of Wine: ' + avenger.wine_served() + '\n')
+                
+                #Joe F.: I would maybe add another '\n' to the end of this to give a space between records?
                 ofile.write('* Liters of Pure Alcohol: ' + avenger.total_litres_of_pure_alcohol() + '\n')
