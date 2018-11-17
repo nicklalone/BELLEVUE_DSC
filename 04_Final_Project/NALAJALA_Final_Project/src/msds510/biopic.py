ccf6f2c64b5b826e7b290b96e7d61c91bacd8382
sridnala <43190526+sridnala@users.noreply.github.com>
2018-11-17 08:59:46 -0500

    Week 12: Adding unit tests

--- 04_Final_Project/NALAJALA_Final_Project/src/msds510/biopic.py
+++ 04_Final_Project/NALAJALA_Final_Project/src/msds510/biopic.py
@@ -43,7 +43,7 @@ class Biopic:
         """
         return self.site_value
 
-    def country(self):
+    def get_country(self):
         """
         Returns: str: The countries biopic release in
         """
@@ -61,26 +61,25 @@ class Biopic:
         """
         return float(self.box_office_amount)
 
-    def director(self):
+    def get_director(self):
         """
         Returns: str: The full name of the director
         """
         return self.director.strip()
 
-    def number_of_subjects(self):
+    def get_number_of_subjects(self):
         """
         Returns:int: The number of subjects that biopic based on
         """
         return int(self.number_of_subjects)
 
-    def subject(self):
+    def get_subject(self):
         """
-
-Returns: str: The main subject that biopic based on
+        Returns: str: The main subject that biopic based on
         """
         return self.subject.strip()
 
-    def type_of_subject(self):
+    def get_type_of_subject(self):
         """
         Returns: str: The type of subject that biopic based on
         """
@@ -94,24 +93,24 @@ Returns: str: The main subject that biopic based on
         if not self.race_known.strip():
             return None
         else:
-            return True if self.current == 'KNOWN' else False
+            return True if self.race_known.strip() == 'KNOWN' else False
 
-    def subject_race(self):
+    def get_subject_race(self):
         """
         Returns:str: The subject race of the biopic
         """
         return self.subject_race
 
-    def person_of_color(self):
+    def get_person_of_color(self):
         """
         Returns: bool: color of person? (True/False)
         """
         if not self.person_of_color.strip():
             return None
         else:
-            return True if self.current == '1' else False
+            return True if self.person_of_color.strip() == '1' else False
 
-    def lead_actor_actress(self):
+    def get_lead_actor_actress(self):
         """
         Returns:str: Actress name in the biopic
         """
@@ -138,6 +137,7 @@ Returns: str: The main subject that biopic based on
                                      for key, val in self.record.items()
                                      if key == 'title'
                                      or key == 'site') + ")"
+
     def to_markdown(self, recordslist, outfile):
         """takes a list of records, formats them and prints them to an output file.
         Args:
@@ -151,9 +151,9 @@ Returns: str: The main subject that biopic based on
                 for idx, rc in enumerate(recordslist):
                     biopic = Biopic(rc)
                     ofile.write('#  %d. %s\n' % (idx + 1, biopic.title()))
-                    ofile.write('*  Director: %s \n' % biopic.director)
+                    ofile.write('*  Director: %s \n' % biopic.get_director())
                     ofile.write('*  Year Released: %d \n' % biopic.year_release())
-                    ofile.write('*  Countries Released: %s \n' % biopic.country)
+                    ofile.write('*  Countries Released: %s \n' % biopic.get_country())
                     ofile.write('*  Box Office: $%s million\n' % format(biopic.box_office(), ",.2f"))
                     ofile.write('*  Years Since Released: %d \n' % biopic.years_since_released())
                     ofile.write('*  URL: <%s>\n' % biopic.site())
